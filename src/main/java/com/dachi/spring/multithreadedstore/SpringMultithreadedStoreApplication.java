885f1dec96629955770621197229e7f50b4edf00
Iamdachi <d_zedginidze@cu.edu.ge>
2025-11-13 22:42:19 +0400

    spring set up

---
+++ src/main/java/com/dachi/spring/multithreadedstore/SpringMultithreadedStoreApplication.java
@@ -0,0 +1,13 @@
+package com.dachi.spring.multithreadedstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMultithreadedStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMultithreadedStoreApplication.class, args);
+    }
+
+}
